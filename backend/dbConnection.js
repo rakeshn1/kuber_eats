fda0a76b1635d0006cc70d8123b8492af8fe1838
Rakesh Nagarajappa <78629375+rakeshn1@users.noreply.github.com>
2021-09-13 08:35:46 -0700

    Changing the mysql npm to mysql 2 to support async/await

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -1,14 +1,18 @@
+'use strict';
 /*
 Mysql DB connection
  */
-const mysql = require('mysql');
 const config = require('./config.json')
-var pool  = mysql.createPool({
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
     connectionLimit : 100,
     host            : config.dbConfiguration.host,
     user            : config.dbConfiguration.userName,
     password        : config.dbConfiguration.password,
     database        : config.dbConfiguration.database
 });
+const promisePool = pool.promise();
 
-module.exports = pool;
+
+module.exports = promisePool;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,33 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const pool = require('../dbConnection');
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  pool.getConnection(function(err, connection) {
-    if (err) {
-      console.error("DB connection failed with following error:")
-      console.error(err)
-      res.status(400).json({
-        msg: "DB connection failed: "+err
-      })
+router.get('/', async function(req, res, next) {
+  try{
+    const [rows ,fields] = await pool.query("SELECT * FROM users");
+    if(rows){
+      console.log("Fetched the data from DB")
+      res.status(200).json(rows[0]);
     }
+  }catch (e) {
+    console.error("Error fetching data from DB:")
+    console.error(e)
+    res.status(400).json({
+              msg: "Error fetching data from DB: "+e
+            })
+  }
+});
 
-    connection.query('SELECT * FROM users', function (error, results, fields) {
-      connection.release();
-      if (error) {
-        console.error("Error fetching data from DB:")
-        console.error(error)
-        res.status(400).json({
-          msg: "Error fetching data from DB: "+err
-        })
-      }
-      if(results.length > 0){
-        res.status(200).json(results[0]);
-      }
-    });
-  });
+/*Check Login credentials*/
+router.post('/create', function(req, res, next) {
+
 });
 
 module.exports = router;
