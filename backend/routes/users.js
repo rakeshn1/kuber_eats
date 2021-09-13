51a2552e1051ac0d8a7a5e0697704d35b117582e
Rakesh Nagarajappa <78629375+rakeshn1@users.noreply.github.com>
2021-09-13 11:47:21 -0700

    Password encryption and API for login

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,14 +4,15 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const pool = require('../dbConnection');
 
+const selectQuery = "SELECT * FROM users";
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
   try{
-    const [rows ,fields] = await pool.query("SELECT * FROM users");
+    const [rows ,fields] = await pool.query(selectQuery);
     if(rows){
       console.log("Fetched the data from DB")
-      res.status(200).json(rows[0]);
+      res.status(200).json(rows);
     }
   }catch (e) {
     console.error("Error fetching data from DB:")
@@ -22,9 +23,62 @@ router.get('/', async function(req, res, next) {
   }
 });
 
-/*Check Login credentials*/
-router.post('/create', function(req, res, next) {
+/*Create user*/
+router.post('/create', async function(req, res, next) {
+  try{
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
+    let sqlQuery = "INSERT users VALUES ("+req.body.id+",'"+ req.body.name +"','"+ hashedPassword +"',"+ req.body.number +","+ req.body.email +")";
+    console.log(sqlQuery)
+    const [rows] = await pool.query(sqlQuery);
+    console.log(rows)
+    if(rows.affectedRows){
+      res.status(200).json({msg: "Successfully created user"});
+    }else{
+      throw new Error("DB didn't return success response");
+    }
+  }catch (e) {
+    console.error("Error creating the user:")
+    console.error(e)
+    res.status(400).json({
+      msg: "Error creating the user: "+e
+    })
+  }
+});
+
+/*Check Login credentials*/
+router.post('/login', async function(req, res, next) {
+  try{
+    const [rows] = await pool.query(selectQuery);
+    let flag = false
+    if(rows.length > 0){
+      for(let i = 0; i < rows.length; i++){
+        let row = rows[i];
+        if(row.name === req.body.name){
+          let result = await bcrypt.compare(req.body.password, row.Password);
+          if(result){
+            flag = true;
+            break;
+          }
+        }
+      }
+      if(flag){
+        console.log("User credentials are valid");
+        res.status(200).json({msg: "User credentials are correct"});
+      }else{
+        console.log("User credentials are Invalid");
+        res.status(400).json({msg: "User name or password is invalid"});
+      }
+    }else{
+      res.status(400).json({msg: "No users  present"});
+    }
+  }catch (e) {
+    console.error("Error checking login credentials:")
+    console.error(e)
+    res.status(400).json({
+      msg: "Error checking login credentials: "+e
+    })
+  }
 });
 
 module.exports = router;
