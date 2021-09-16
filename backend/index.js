8d993afb1fae11d920ecbb69ddcbbe4b79a8688e
Rakesh Nagarajappa <78629375+rakeshn1@users.noreply.github.com>
2021-09-15 17:40:08 -0700

    Handling CORS issue from backend

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,11 @@ app.use(session({
     saveUninitialized: true
 }));
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use('/users', usersRouter);
 
