9d2a1bbbe582ef803ed58e89f23aa64e541faff5
Rakesh Nagarajappa <78629375+rakeshn1@users.noreply.github.com>
2021-09-15 17:27:10 -0700

    Frontend initial commit with Login page

---
+++ frontend/src/App.js
@@ -0,0 +1,92 @@
+import React from "react";
+import "./App.css";
+import { Header } from "./components/Header/Header.jsx";
+import { Main } from "./components/Main/Main.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { RestaurantPage } from "./components/Restaurant-page/Restaurant-page.jsx";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import { Basket } from "./components/Basket/Basket";
+import Login from "./components/LoginPage/login";
+
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      basketOrders:
+        JSON.parse(window.localStorage.getItem("basketOrders")) || [],
+      isBasketOpen: false,
+      isLoggedIn : false
+    };
+  }
+
+  setOpenBasket = () => {
+    this.setState({ isBasketOpen: !this.state.isBasketOpen });
+  };
+
+  addToBasket = newOrderArray => {
+    this.setState({ basketOrders: [...newOrderArray] });
+    window.localStorage.setItem(
+      "basketOrders",
+      JSON.stringify([...newOrderArray])
+    );
+  };
+
+  setIsLoggedIn = () => {
+    this.setState({ isLoggedIn: true });
+  };
+
+  removeFromBasket = dish => {
+    let newArray = this.state.basketOrders.filter(basketOrder => {
+      return basketOrder !== dish;
+    });
+    this.setState({
+      basketOrders: [...newArray]
+    });
+    window.localStorage.setItem("basketOrders", JSON.stringify([...newArray]));
+  };
+
+  renderRestaurantPage = props => {
+    return (
+      <RestaurantPage
+        match={props.match}
+        basketOrders={this.state.basketOrders}
+        addToBasket={this.addToBasket}
+      />
+    );
+  };
+
+  render() {
+    if(!this.state.isLoggedIn){
+      return(
+          <Login setIsLoggedIn={this.setIsLoggedIn}/>
+      )
+    }else{
+      return (
+          <>
+            <Router>
+              <ScrollToTop>
+                {this.state.isBasketOpen && (
+                    <Basket
+                        basketOrders={this.state.basketOrders}
+                        setOpenBasket={this.setOpenBasket}
+                        removeFromBasket={this.removeFromBasket}
+                        addToBasket={this.addToBasket}
+                    />
+                )}
+                <Header open={this.setOpenBasket} />
+                <Route path="/" exact component={Main} />
+                <Route
+                    path="/restaurant-page/:id"
+                    component={this.renderRestaurantPage}
+                />
+              </ScrollToTop>
+            </Router>
+            <Footer />
+          </>
+      );
+    }
+  }
+}
+
+export default App;
---
+++ frontend/src/components/LoginPage/login.js
@@ -0,0 +1,81 @@
+import React from "react"
+import "./login.css";
+const axios = require('axios');
+
+export default function Login(props){
+    async function handleClick(event){
+        try{
+            event.preventDefault();
+            const response = await axios({
+                method: 'post',
+                url: 'http://localhost:5676/users/login',
+                data: {
+                    "name": event.target.username.value,
+                    "password": event.target.password.value
+                }
+            });
+            let q =1;
+            if(response.status == 200){
+                props.setIsLoggedIn();
+            }else{
+                alert(response.data)
+            }
+        }catch (e) {
+            console.log(e)
+            alert(e)
+        }
+    }
+    return(
+        <>
+            <div className="limiter">
+                <div className="container-login100">
+                    <div className="wrap-login100 p-t-85 p-b-20">
+                        <form className="login100-form validate-form" onSubmit={handleClick}>
+					<span className="login100-form-title p-b-70">
+						Welcome back
+					</span>
+                            <div className="wrap-input100 validate-input m-t-85 m-b-35" validate="Enter username">
+                                <input className="input100" type="text" name="username" placeholder="Username"/>
+                                <span className="focus-input100" placeholder="Username"></span>
+                            </div>
+
+                            <div className="wrap-input100 validate-input m-b-50" validate="Enter password">
+                                <input className="input100" type="password" name="password" placeholder="Password"/>
+                                    <span className="focus-input100" placeholder="Password"></span>
+                            </div>
+                            <br/>
+                            <br/>
+                            <div className="container-login100-form-btn">
+                                <button className="login100-form-btn">
+                                    Login
+                                </button>
+                            </div>
+                            <br/><br/>
+                            <ul className="login-more p-t-190">
+                                <li className="m-b-8">
+							<span className="txt1">
+								Forgot
+							</span>
+
+                                    <a href="#" className="txt2">
+                                        Username / Password?
+                                    </a>
+                                </li>
+
+                                <li>
+							<span className="txt1">
+								Don’t have an account?
+							</span>
+
+                                    <a href="#" className="txt2">
+                                        Sign up
+                                    </a>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
