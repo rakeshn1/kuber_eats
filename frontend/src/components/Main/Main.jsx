f84e3ba66def93dbba6abf7cd63f24f6f2323fa5
Rakesh Nagarajappa <78629375+rakeshn1@users.noreply.github.com>
2021-09-30 13:33:45 -0700

    Adding user dashboard page

---
+++ frontend/src/components/Main/Main.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import "./Main.css";
+import { Search } from "../Search/Search";
+import RestaurantChoose from "../Restaurants-choose/Restaurants-choose";
+import { Container } from "../../Container/Container";
+
+export class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: "",
+      restaurants: []
+    };
+    this.updateSearch = this.updateSearch.bind(this);
+  }
+
+  componentDidMount() {
+    (async () => {
+      const response = await fetch(
+        "https://uber-eats-mates.herokuapp.com/api/v1/restaurants"
+      );
+      const loadedRestaurants = await response.json();
+      await this.setState({
+        restaurants: loadedRestaurants
+      });
+    })();
+  }
+
+  updateSearch(input) {
+    this.setState(state => ({
+      searchValue: input
+    }));
+  }
+
+  ifTagsInclude(restaurant) {
+    for (let i = 0; i < restaurant.tags.length; i++) {
+      if (
+        restaurant.tags[i].name
+          .toLowerCase()
+          .includes(this.state.searchValue.toLocaleLowerCase())
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  ifCategoriesInclude(restaurant) {
+    for (let i = 0; i < restaurant.categories.length; i++) {
+      if (
+        restaurant.categories[i].name
+          .toLowerCase()
+          .includes(this.state.searchValue.toLocaleLowerCase())
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  render() {
+    return (
+      <Container>
+        <main className="Main">
+          <Search updateSearch={this.updateSearch} />
+          <p className="Main__city">Restaurants</p>
+          <div className="Main__restaurants-list">
+            {this.state.restaurants.length > 0
+              ? this.state.restaurants
+                  .filter((restaurant, i) => {
+                    return (
+                      restaurant.title
+                        .toLowerCase()
+                        .includes(this.state.searchValue.toLocaleLowerCase()) ||
+                      (restaurant.tags && this.ifTagsInclude(restaurant)) ||
+                      this.ifCategoriesInclude(restaurant)
+                    );
+                  })
+                  .map((restaurant, i) => {
+                    return (
+                      <RestaurantChoose
+                        key={i}
+                        title={restaurant.title}
+                        categories={restaurant.categories}
+                        priceBucket={restaurant.priceBucket}
+                        imageUrl={restaurant.imageUrl}
+                        etaRange={restaurant.etaRange}
+                        uuid={restaurant.uuid}
+                      />
+                    );
+                  })
+              : ""}
+          </div>
+        </main>
+      </Container>
+    );
+  }
+}
---
+++ frontend/src/components/Restaurants-choose/Restaurants-choose.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import "./Restaurants-choose.css";
+import { Link } from "react-router-dom";
+import { changeFavorite } from "../../redux/user";
+import { connect } from "react-redux";
+import axios from "axios";
+import { BACKEND_HOST, BACKEND_PORT } from "../../config";
+import Swal from "sweetalert2";
+
+function RestaurantChoose(props) {
+  const likedImageUrl = "http://localhost:5676/images/like.png";
+  const unLikedImageUrl = "http://localhost:5676/images/heart.png";
+  const [imageUrl, setImageUrl] = useState(unLikedImageUrl);
+
+  const handleClick = async e => {
+    e.preventDefault();
+    try {
+      let updatedData = {
+        favorites: props.userData.favorites,
+        id: props.userData.id,
+        restaurantID: props.uuid
+      };
+      const response = await axios({
+        method: "put",
+        url: `http://${BACKEND_HOST}:${BACKEND_PORT}/users/updateFavorites`,
+        data: updatedData
+      });
+      if (response.status == 200) {
+        props.changeFavorite(props.uuid);
+      } else {
+        throw new Error(response.data.msg);
+      }
+    } catch (e) {
+      console.log(e);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: e
+      });
+    }
+  };
+
+  return (
+    <div className="Restaurants-choose">
+      <Link
+        to={`restaurant-page/${props.uuid}`}
+        className="Restaurants-choose__link"
+      >
+        <div className="restaurants-choose__header">
+          <img
+            src={props.imageUrl}
+            className="Restaurants-choose__photo"
+            alt={props.title}
+          />
+        </div>
+        {props.userData.favorites.includes(props.uuid) && (
+          <img
+            title={"Remove from favorites"}
+            className="fa-heart"
+            src={likedImageUrl}
+            onClick={handleClick}
+          />
+        )}
+        {!props.userData.favorites.includes(props.uuid) && (
+          <img
+            title={"Save as favorite"}
+            className="fa-heart"
+            src={unLikedImageUrl}
+            onClick={handleClick}
+          />
+        )}
+        <div className="restaurants-choose__footer">
+          <p className="Restaurants-choose__name">{props.title}</p>
+          <p className="Restaurants-choose__about">
+            {props.priceBucket}{" "}
+            {props.categories.map(category => {
+              return ` • ${category.name}`;
+            })}{" "}
+          </p>
+          <p className="Restaurants-choose__delivery-time">
+            {props.etaRange.min} - {props.etaRange.max} min
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+function mapStateToProps(globalState) {
+  return {
+    userData: globalState.user,
+    isUserLoggedIn: globalState.isUserLoggedIn
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    changeFavorite: uuid => dispatch(changeFavorite(uuid))
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RestaurantChoose);
